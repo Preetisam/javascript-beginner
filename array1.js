bc44549d927dc2f605acf8f60fb7532ce91d9411
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-02-21 14:26:58 +0530

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,35 @@
+let a =[1,2,3,4,"preeti",false]
+console.log(a);
+let b = new Array("abc", "def","ghi");
+console,log(b)
+
+Array is the collection of data
+
+
+array are called Zero based indexing array
+let a = [1, 2, 3, 4, "preeti", false];
+console.log(a);
+let b = new Array("abc", "def", "ghi");
+console.log(b);
+
+// Array  are the collection of the data which can be encolsed in the Square brackets
+// there are two ways to declare the array in javascript .
+// one with the help of square brackets and another with the help of thr new  Arrray key word
+//in array the elements are placed in the positions but we call them indexies.array conatins a uinque poisition these are called indexes
+//the element in the first indexsies is denoted as arr[0]
+//the first element in postion 1 and index=0
+//the last index =arr.length
+//the element last element as the arr.length-1
+
+// uses of array 
+// Grouping related to arrays
+//Storing large amount of data
+//Imporving performance
+//Ease to use
+//Better Readibility
+
+console.log(a[4]);
+a[4] = 5;
+
+// Array in javascriot are mutable
+console.log(a); //[ 1, 2, 3, 4, 5, false ] upadted array
